d5d16895dca52f7e34910f6ec69cd2e3cd7681c5
Joe-Newm <j.newm500@gmail.com>
2025-05-16 23:48:51 -0500

    init

---
+++ 02-backend/ecommerce/src/main/java/com/spring/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,28 @@
+package com.spring.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name="product_category")
+//@Data
+@Getter
+@Setter
+public class ProductCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name="category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    @JsonIgnore
+    private Set<Product> products;
+}
